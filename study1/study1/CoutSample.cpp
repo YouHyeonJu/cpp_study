badeaefbfb9eedc461c9a2b8dc3fd704a6211b69
maettuki <dbguswn001@naver.com>
2021-04-08 18:00:21 +0900

    bug...

---
+++ study1/study1/CoutSample.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+int main()
+{
+	std::cout << 10 << std::endl;
+	std::cout << 10U << std::endl;
+	std::cout << 10.5F << std::endl;
+	std::cout << 10.5 << std::endl;
+	std::cout << 3+4 << std::endl;
+	return 0;
+}
---
+++ study1/study1/CoutString.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <iostream>
+
+int main() {
+	std::string strData = "test string";
+	std::cout << "sample string" << std::endl;
+	std::cout << strData << std::endl;
+	
+	strData = "New string";
+	std::cout << strData << std::endl;
+	std::cout << "저는" << 20 << "살" << "입니다" << std::endl;
+}
