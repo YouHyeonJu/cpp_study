152e6a08aff1fdba97f426f69e6db0d4e76b56f8
maettuki <dbguswn001@naver.com>
2021-04-16 23:42:45 +0900

    .

--- study2/study2/study2/namespace.cpp
+++ study2/study2/study2/namespace.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
 
 using namespace std;
-template <typename T>
-T Add(T a, T b) {
-	return a + b;
-}
 
-int main() {
-	cout << Add(3, 4) << endl;
-	cout << Add(3.3, 4.4) << endl;
+int nData(20);
 
+int main(int argc) {
+	int nData(10);
+	cout << nData << endl;
+	cout << argc << endl;
+	
 }
